a611e220c368138b588f7345695e4fd313d1bfe4
Alex Miller <asmiller1989@gmail.com>
2021-02-02 13:24:30 -0600

    Gets BTC export working

--- cmd/exportBTCKey.go
+++ cmd/exportBTCKey.go
@@ -23,8 +23,10 @@ import (
 	"github.com/spf13/cobra"
 )
 
-var privateKeysMode bool
+var electrumPrefixes bool
+var electrumWallet bool
 var numKeys int
+var startPath string
 
 // exportBTCKeyCmd represents the exportBTCKey command
 var exportBTCKeyCmd = &cobra.Command{
@@ -37,10 +39,6 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		if privateKeysMode {
-			fmt.Println("selected private keys option")
-			return
-		}
 		exportBTCKey()
 	},
 }
@@ -57,8 +55,22 @@ func init() {
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
 	// exportBTCKeyCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
-	exportBTCKeyCmd.Flags().BoolVar(&privateKeysMode, "private-keys", false, "export individual BTC private keys")
-	exportBTCKeyCmd.Flags().IntVar(&numKeys, "num-keys", 1, "the number of private keys to export")
+	exportBTCKeyCmd.Flags().BoolVar(&electrumWallet,
+		"electrum-master-priv",
+		false,
+		"(Default false) If true, export a single BIP49 master private key which may be imported into Electrum")
+	exportBTCKeyCmd.Flags().IntVar(&numKeys,
+		"num-keys",
+		1,
+		"(Default 1) Number of private keys to export")
+	exportBTCKeyCmd.Flags().StringVar(&startPath,
+		"start-path",
+		"m/49'/0'/0'/0/0",
+		"(Default m/49'/0'/0'/0/0) A BIP32 path providing the starting point of the derivation")
+	exportBTCKeyCmd.Flags().BoolVar(&electrumPrefixes,
+		"electrum-prefixes",
+		false,
+		"(Default false) If true, include prefixes on exported individual private keys that allows them to be properly imported into Electrum.")
 }
 
 func exportBTCKey() {
@@ -67,6 +79,41 @@ func exportBTCKey() {
 		return
 	}
 
+	// Export individual private key(s)
+	if false == electrumWallet {
+		path, err := crypto.GetPath(startPath)
+		if err != nil {
+			fmt.Println("Error encountered parsing path: ", err)
+			return
+		}
+		currency, err := crypto.GetCurrencyType(path)
+		if err != nil {
+			fmt.Println("Error encountered with path: ", err)
+		}
+		fmt.Println("\n-------------------------")
+		fmt.Printf("Exporting %d %s private keys\n", numKeys, currency)
+		fmt.Printf("Start path: %s\n", startPath)
+		fmt.Println("-------------------------")
+		fmt.Println()
+		for i := 0; i < numKeys; i++ {
+			priv, err := crypto.DerivePrivateKey(seed, path)
+			if err != nil {
+				fmt.Println("Error encountered deriving key: ", err)
+				return
+			}
+			prefix, err := crypto.GetElectrumPrivKeyPrefix(path)
+			if err != nil {
+				fmt.Println("Error encountered buidling Electrum import string: ", err)
+			}
+			fmt.Printf("%s%s\n", prefix, priv)
+			path[len(path)-1]++
+		}
+		fmt.Println()
+
+		return
+	}
+
+	// Otherwise we are exporting the Electrum master private key
 	privateKey, err := crypto.GetElectrumBIP49MasterPriv(seed)
 	if err != nil {
 		return
--- crypto/crypto.go
+++ crypto/crypto.go
@@ -2,7 +2,10 @@ package crypto
 
 import (
 	"encoding/hex"
+	"errors"
 	"fmt"
+	"strconv"
+	"strings"
 
 	hdwallet "github.com/miguelmota/go-ethereum-hdwallet"
 	log "github.com/sirupsen/logrus"
@@ -43,6 +46,24 @@ func DeriveEthWalletPrivateKey(seed []byte) (address string, privKey string, err
 	return account.Address.Hex(), privateKey, nil
 }
 
+// DerivePrivateKey derives a single HD wallet's private key given the seed and a path of indices.
+// Returns hex string representation of private key
+func DerivePrivateKey(seed []byte, path []uint32) (privKey string, err error) {
+	key, err := bip32.NewMasterKey(seed)
+	if err != nil {
+		log.Error("error deriving master private key from seed: ", err)
+		return "", err
+	}
+	for i := 0; i < len(path); i++ {
+		key, err = key.NewChildKey(path[i])
+		if err != nil {
+			return "", err
+		}
+	}
+	keyStr := hex.EncodeToString(key.Key)
+	return keyStr, nil
+}
+
 // GetElectrumBIP49MasterPriv exports a BIP49 master private key for import into Electrum.
 // Generally, the master private key would be the `masterKey` defined below.
 // However, Electrum requires further derivation: m/49'/0'/0'. They consider this derived
@@ -53,6 +74,7 @@ func GetElectrumBIP49MasterPriv(seed []byte) (privKey string, err error) {
 	masterKey, err := bip32.NewMasterKey(seed)
 	if err != nil {
 		log.Error("error deriving master private key from seed: ", err)
+		return "", err
 	}
 	// Replace the key version
 	masterKey.Version = bip49Version()
@@ -75,4 +97,65 @@ func GetElectrumBIP49MasterPriv(seed []byte) (privKey string, err error) {
 	}
 	third.Version = bip49Version()
 	return third.B58Serialize(), nil
+}
+
+// GetElectrumPrivKeyPrefix returns a string containing a prefix that must be included
+// when a user imports a private key string into Electrum (Bitcoin wallet).
+// The Lattice uses BIP49 derivations, so we expect 49' most of the time, though 44'
+// is also supported (legacy p2pkh, so there is no prefix needed).
+// Otherwise an error is thrown.
+func GetElectrumPrivKeyPrefix(path []uint32) (prefix string, err error) {
+	switch path[0] {
+	case 49 + 0x80000000:
+		return "p2wpkh-p2sh:", nil // Wrapped segwit (default)
+	case 44 + 0x80000000:
+		return "", nil // Default p2pkh
+	default:
+		return "", errors.New("unsupported path type. Must be 49' or 44'")
+	}
+}
+
+// GetPath takes a path string (e.g. m/44'/0'/0'/0/0) and converts it to uint32 indices
+func GetPath(pathStr string) ([]uint32, error) {
+	indices := strings.Split(pathStr, "/")
+	start := 0
+	if indices[0] == "m" {
+		start = 1
+	}
+	path := make([]uint32, (len(indices) - start))
+	for i := start; i < len(indices); i++ {
+		_strIdx := indices[i]
+		_L := len(indices[i])
+		isHardened := string(indices[i][(_L-1):]) == "'"
+		strIdx := _strIdx
+		if isHardened {
+			strIdx = _strIdx[:(_L - 1)]
+		}
+		idx, err := strconv.ParseUint(strIdx, 10, 32)
+		if err != nil {
+			return nil, err
+		}
+		if isHardened {
+			idx += 0x80000000
+		}
+		path[i-start] = uint32(idx)
+	}
+	return path, nil
+}
+
+// GetCurrencyType takes a path (set of u32 indices) and returns the name of the
+// currency, if applicable. Returns error if currency is not supported or path
+// is malformed.
+func GetCurrencyType(path []uint32) (name string, err error) {
+	if len(path) < 2 {
+		return "", errors.New("path contains <2 indices")
+	}
+	switch path[1] {
+	case 0x80000000:
+		return "Bitcoin", nil
+	case 60 + 0x80000000:
+		return "Ethereum", nil
+	default:
+		return "", errors.New("currency must be either 0' (Bitcoin) or 60' (Ethereum)")
+	}
 }
